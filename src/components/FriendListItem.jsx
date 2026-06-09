19a11c7bea21412ccaae5f332ea4f283fb99da1c
MrDonex <sylenko.matvii@icloud.com>
2026-06-09 12:23:08 +0300

    rady

---
+++ src/components/FriendListItem.jsx
@@ -0,0 +1,9 @@
+export const FriendListItem = ({ avatar, name, isOnline }) => {
+  return (
+    <li class="item">
+      <span class={`status ${isOnline ? 'online' : 'offline'}`}></span>
+      <img class="avatar" src={avatar} alt={name} width="48" />
+      <p class="name">{name}</p>
+    </li>
+  );
+};
---
+++ src/components/Statistics.jsx
@@ -0,0 +1,16 @@
+export const Statistics = ({ stats }) => {
+  return (
+    <section class="statistics">
+      <h2 class="title">Upload stats</h2>
+
+      <ul class="stat-list">
+        {stats.map((item) => (
+          <li key={item.id} class="item">
+            <span class="label">{item.label}</span>
+            <span class="percentage">{item.percentage}</span>
+          </li>
+        ))}
+      </ul>
+    </section>
+  );
+};
