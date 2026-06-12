db096c84f4b53d30a90dfb861dad96d01667d708
MrDonex <sylenko.matvii@icloud.com>
2026-06-12 10:13:54 +0300

    gfgg

--- src/components/FriendListItem.jsx
+++ src/components/FriendListItem.jsx
@@ -1,6 +1,6 @@
 export const FriendListItem = ({ avatar, name, isOnline }) => {
   return (
-    <li class="item">
+    <li class="item-friend">
       <span class={`status ${isOnline ? 'online' : 'offline'}`}></span>
       <img class="avatar" src={avatar} alt={name} width="48" />
       <p class="name">{name}</p>
--- src/components/Statistics.jsx
+++ src/components/Statistics.jsx
@@ -1,11 +1,29 @@
+const getRandomColor = () => {
+  const colors = [
+    '#FF6B6B',
+    '#4ECDC4',
+    '#45B7D1',
+    '#FFA07A',
+    '#98D8C8',
+    '#F7DC6F',
+    '#BB8FCE',
+    '#85C1E2',
+  ];
+  return colors[Math.floor(Math.random() * colors.length)];
+};
+
 export const Statistics = ({ stats }) => {
   return (
     <section class="statistics">
       <h2 class="title">Upload stats</h2>
 
       <ul class="stat-list">
-        {stats.map((item) => (
-          <li key={item.id} class="item">
+        {stats.map(item => (
+          <li
+            key={item.id}
+            class="item"
+            style={{ backgroundColor: getRandomColor() }}
+          >
             <span class="label">{item.label}</span>
             <span class="percentage">{item.percentage}</span>
           </li>
